d3a7ce6f645382ca72eba2a56e5af0dc99a47c81
Yes <yes@ysnt.live>
2026-08-18 17:02:00 -0400

    Moar UI

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,25 +1,33 @@
 <script lang="ts">
-  import { Minus, Square, X } from "@lucide/svelte";
+  import { Menu, Minus, Square, X } from "@lucide/svelte";
   import { Window } from "@wailsio/runtime";
   let color = $state<string>("#9900ff");
   $effect(() => {
     document.documentElement.style.setProperty("--color-brand", color);
   });
+  let theme = $state<"light" | "dark" | "system">("light");
+  $effect(() => {
+    let value = theme === "system" ? "light dark" : theme;
+    document.documentElement.style.setProperty("color-scheme", value);
+  });
 </script>
 
 <div class="modern-titlebar">
+  <button class="sidebar-button">
+    <Menu size={24}/>
+  </button>
   <div class="title">
     <p>YStream<span>Utils</span></p>
   </div>
   <div class="controls">
     <button class="minimize" onclick={() => Window.Minimise()}>
-      <Minus size={14} />
+      <Minus size={16} />
     </button>
     <button class="maximize" onclick={() => Window.ToggleMaximise()}>
-      <Square size={14} />
+      <Square size={16} />
     </button>
     <button class="close" onclick={() => Window.Close()}>
-      <X size={14} />
+      <X size={16} />
     </button>
   </div>
 </div>
@@ -33,13 +41,19 @@
     <option value="#00ff99">Green</option>
     <option value="#ff9900">Orange</option>
   </datalist>
+
+  <select bind:value={theme}>
+    <option value="light">Light</option>
+    <option value="dark">Dark</option>
+    <option value="system">System</option>
+  </select>
 </main>
 
 <style>
   main {
     width: stretch;
     height: stretch;
-    padding: 8px;
+    padding: var(--space-2);
   }
   .modern-titlebar {
     -webkit-app-region: drag;
@@ -48,15 +62,15 @@
     height: 40px;
     background: light-dark(var(--neutral-200-tint), var(--neutral-900-tint));
     border-bottom: 1px solid #1a1a1a;
-    padding: 0 16px;
+    padding: 0 var(--space-4);
   }
 
   .title {
     flex: 1;
-    text-align: center;
+    /*text-align: center;*/
     color: var(--color-text);
     user-select: none;
-    margin-left: 140px; /* Adjust for control buttons */
+    margin-left: var(--space-4);
   }
 
   .title span {
@@ -76,9 +90,26 @@
     border: none;
     background: transparent;
     color: var(--color-text);
+    cursor: pointer;
+    transition: background 0.2s;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .sidebar-button {
+    -webkit-app-region: no-drag;
+    width: 32px;
+    height: 32px;
+    border: none;
+    background: transparent;
+    color: var(--color-text);
     font-size: 14px;
     cursor: pointer;
     transition: background 0.2s;
+    padding: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 
   .controls button.minimize {
@@ -93,7 +124,7 @@
     --wails-non-client-region: close;
   }
 
-  .controls button:hover {
+  .controls button:hover, .sidebar-button:hover {
     background: rgba(255, 255, 255, 0.1);
   }
 
